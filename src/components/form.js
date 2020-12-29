cff7302c095b97958851a7a37c904b188afb658e
Artem Zymovets <artwayprojects@gmail.com>
2020-12-30 00:39:09 +0200

    users validation

--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import shortid from 'shortid'
-import Contacts from './contacts'
+import Section from './section'
+
 const INITIAL_STATE = {
     contacts: [
         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -18,22 +19,42 @@ class Form extends Component {
     }
 
     number = shortid.generate()
-
+    invalid = (name) => {
+        const foundUsers = this.state.contacts.filter(item => item.name === name)
+        if (foundUsers.length > 0) {
+            return alert('user already exists')
+        }
+        return false
+    }
     addContact = e => {
         e.preventDefault()
         const { name, number } = e.target
+
+        if (this.invalid(name.value)) return
+
+
         const contact = {
             "name": name.value,
             "number": number.value,
             "id": shortid.generate()
         }
+
         this.setState({
             name: name.value,
             number: number.value,
             contacts: [...this.state.contacts, contact]
         })
     }
+    changeFilter = (e) => {
+        const { value } = e.target
 
+        this.setState({ ...this.state, filter: value.toLowerCase() })
+    }
+    filterResults = (contacts) => {
+        return contacts.filter(item => item.name.toLowerCase().includes(this.state.filter)).map(item =>
+            <div key={shortid.generate()}> {item.name} {item.number}</ div>)
+
+    }
     render() {
 
         return (
@@ -53,9 +74,18 @@ class Form extends Component {
 
                     <button type="submit">Add contact</button>
                 </form>
-                <Contacts state={this.state} />
-            </div>
 
+                <Section title="Contacts">
+
+
+                    <label htmlFor="filter">
+                        <input type="text" id="filter" onChange={this.changeFilter} />
+                    </label>
+
+
+                    {this.filterResults(this.state.contacts)}
+                </Section>
+            </div>
         )
     }
 }
