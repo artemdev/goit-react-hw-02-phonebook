1b8f37368042eaf6072184de460112cd2685d994
Artem Zymovets <artwayprojects@gmail.com>
2020-12-29 22:20:52 +0200

    filter numbers

---
+++ src/components/filter.js
@@ -0,0 +1,15 @@
+// import { Component } from 'react'
+
+// class Filter extends Component {
+//     filterNames = () => {
+
+//     }
+//     render() {
+//         return (
+
+
+//         )
+//     }
+// }
+
+// export default Filter
